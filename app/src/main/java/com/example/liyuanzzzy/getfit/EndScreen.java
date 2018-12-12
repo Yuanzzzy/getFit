4b6428f393e45507e736dd9403fa33019ef7c333
liyuanzzzy <lyang58@illinois.edu>
2018-12-12 00:07:35 -0600

    UI -> recommit///

--- app/src/main/java/com/example/liyuanzzzy/getfit/EndScreen.java
+++ app/src/main/java/com/example/liyuanzzzy/getfit/EndScreen.java
@@ -1,13 +1,37 @@
 package com.example.liyuanzzzy.getfit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class EndScreen extends AppCompatActivity {
+public class EndScreen extends AppCompatActivity implements View.OnClickListener {
+    Button goAgain, treatYrself;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
+
+        goAgain = findViewById(R.id.button_goagain);
+        treatYrself = findViewById(R.id.button_treatYourself);
+        //set up OnClickListeners
+        goAgain.setOnClickListener(this);
+        treatYrself.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_goagain:
+                Intent i = new Intent(EndScreen.this, SetTime.class);
+                startActivity(i);
+                break;
+            case R.id.button_treatYourself:
+                i = new Intent(EndScreen.this, LastPage.class);
+                startActivity(i);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/liyuanzzzy/getfit/LastPage.java
@@ -0,0 +1,21 @@
+package com.example.liyuanzzzy.getfit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class LastPage extends AppCompatActivity {
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_last_page);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.chowbus.com/champaign/");
+    }
+}
